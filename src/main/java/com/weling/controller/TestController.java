1132f2e2690fc86f8a96f263d3444434beff0136
우성호 <90843247+seongho-w@users.noreply.github.com>
2021-12-24 03:01:30 +0900

    Update TestController.java

--- src/main/java/com/weling/controller/TestController.java
+++ src/main/java/com/weling/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test(){
-        String testStr = "Hi~~";
+        String testStr = "Hi~~~";
         System.out.println(testStr);
         return testStr;
     }
